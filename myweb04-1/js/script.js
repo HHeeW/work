e8f4c68123060a2f47d639757b96943eef078167
HeeWon <sun4472@naver.com>
2022-03-08 15:37:24 +0900

    update

--- myweb04-1/js/script.js
+++ myweb04-1/js/script.js
@@ -1,39 +1,26 @@
-function openNav(){
-    document.getElementById("mynav").style.left="0%";
-}
-function closeNav(){
-    document.getElementById("mynav").style.left="-100%";
-}
-
 var ind = 1;
 viewSlide(ind)
 function pSlide(n){
     viewSlide(ind += n);
 }
+
+setInterval(autoSlide, 5000);
+
 function viewSlide(n){
     var i,slides;
     slides = document.getElementsByClassName("slidein");
-    if(n> slides.length) ind = 1;
-    if(n<1) ind = slides.length;
+    if(n > slides.length) ind = 1;
+    if(n < 1) ind = slides.length;
     for(i=0; i<slides.length; i++){
         slides[i].style.display = "none";
+        slides[i].className = "slidein";
     }
+ 
     slides[ind-1].style.display = "block";
+    slides[ind-1].className += " fade";
 }
 
-
-var num = 1;
-function slide(n){
-    if(n){
-        document.getElementById("slide"+num).style.display ='none';
-        num++;
-        if(num == 3){num = 1;}
-        document.getElementById("slide"+num).style.display = "block";
-    }
-    else{
-        document.getElementById("slide"+num).style.display ='none';
-        num--;
-        if(num == 0){num = 2;}
-        document.getElementById("slide"+num).style.display = "block";
-    }
+function autoSlide(){
+    ind++;
+    viewSlide(ind);
 }
