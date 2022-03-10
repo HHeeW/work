0e6ba605d5ac09ba2c2a210fec8daf0aa618982c
HeeWon <sun4472@naver.com>
2022-03-10 18:09:11 +0900

    update

--- myweb04-1/js/script.js
+++ myweb04-1/js/script.js
@@ -1,11 +1,21 @@
 var ind = 1;
 viewSlide(ind)
+function gTime(){
+    var dt = new Date();
+    var yr = dt.getFullYear();
+    var mth = dt.getMonth()+1;
+    var gdt = dt.getDate();
+    var hour = dt.getHours();
+    var minu = dt.getMinutes();
+    var sec = dt.getSeconds();
+    document.getElementsByClassName('dt')[0].innerHTML = yr+"."+ mth+"."+ gdt + "<span>"+hour+"시 "+minu+"분 "+sec+"초</span>";
+    };
 function pSlide(n){
     viewSlide(ind += n);
 }
 
 setInterval(autoSlide, 5000);
-
+setInterval(gTime, 1000);
 function viewSlide(n){
     var i,slides;
     slides = document.getElementsByClassName("slidein");
