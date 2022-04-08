338d51950d5bcaed7f02ad58469ef1a01d250b21
HeeWon <sun4472@naver.com>
2022-04-08 15:59:13 +0900

    update

--- Web/HTML/myweb05-1/js/custom.js
+++ Web/HTML/myweb05-1/js/custom.js
@@ -44,3 +44,153 @@ function fadeInOut(elem, disp) {
     }   
 }
 
+/*  select opt */
+function optChange(e) {
+    document.getElementById('total').innerHTML = '';
+    document.getElementById('total-price').innerHTML = 0;
+    let jy14k = [
+                   {key: '', val:'사이즈를 선택하세요.'} ,
+                   {key: '15.5cm+2.5cm||0||14k 옐로우 골드', val: '15.5cm+2.5cm'}, 
+                   {key: '16.5cm+2.5cm||0||14k 옐로우 골드', val: '16.5cm+2.5cm'}, 
+                   {key: '17.5cm+2.5cm||3000||14k 옐로우 골드', val: '17.5cm+2.5cm (+3000원)'}, 
+                   {key: '18.5cm+2.5cm||6000||14k 옐로우 골드', val: '15.5cm+2.5cm (+6000원)'}, 
+                   {key: '15.5cm+2.5cm||9000||14k 옐로우 골드', val: '15.5cm+2.5cm (+9000원)'}
+                ];
+    let jp14k = [
+                   {key: '', val:'사이즈를 선택하세요.'} ,
+                   {key: '15.5cm+2.5cm||0||14k 핑크 골드', val: '15.5cm+2.5cm'}, 
+                   {key: '16.5cm+2.5cm||0||14k 핑크 골드', val: '16.5cm+2.5cm'}, 
+                   {key: '17.5cm+2.5cm||3000||14k 핑크 골드', val: '17.5cm+2.5cm (+3000원)'}, 
+                   {key: '18.5cm+2.5cm||6000||14k 핑크 골드', val: '15.5cm+2.5cm (+6000원)'}, 
+                   {key: '15.5cm+2.5cm||9000||14k 핑크 골드', val: '15.5cm+2.5cm (+9000원)'}
+                ];  
+    let jw14k = [
+                    {key: '', val:'사이즈를 선택하세요.'} ,
+                    {key: '15.5cm+2.5cm||0||14k 화이트 골드', val: '15.5cm+2.5cm'}, 
+                    {key: '16.5cm+2.5cm||0||14k 화이트 골드', val: '16.5cm+2.5cm'}, 
+                    {key: '17.5cm+2.5cm||3000||14k 화이트 골드', val: '17.5cm+2.5cm (+3000원)'}, 
+                    {key: '18.5cm+2.5cm||6000||14k 화이트 골드', val: '15.5cm+2.5cm (+6000원)'}, 
+                    {key: '15.5cm+2.5cm||9000||14k 화이트 골드', val: '15.5cm+2.5cm (+9000원)'}
+                 ];                      
+    let jy18k = [
+        {key: '', val:'사이즈를 선택하세요.'},
+        {key: '15.5cm+2.5cm||41900||18k 옐로우 골드', val: '15.5cm+2.5cm (+41900원)'}, 
+        {key: '16.5cm+2.5cm||41900||18k 옐로우 골드', val: '16.5cm+2.5cm (+41900원)'}, 
+        {key: '17.5cm+2.5cm||45900||18k 옐로우 골드', val: '17.5cm+2.5cm (+45900원)'}, 
+        {key: '18.5cm+2.5cm||49900||18k 옐로우 골드', val: '15.5cm+2.5cm (+49900원)'}, 
+        {key: '15.5cm+2.5cm||53900||18k 옐로우 골드', val: '15.5cm+2.5cm (+53900원)'}
+    ];
+    let jp18k = [
+        {key: '', val:'사이즈를 선택하세요.'},
+        {key: '15.5cm+2.5cm||41900||18k 핑크 골드', val: '15.5cm+2.5cm (+41900원)'}, 
+        {key: '16.5cm+2.5cm||41900||18k 핑크 골드', val: '16.5cm+2.5cm (+41900원)'}, 
+        {key: '17.5cm+2.5cm||45900||18k 핑크 골드', val: '17.5cm+2.5cm (+45900원)'}, 
+        {key: '18.5cm+2.5cm||49900||18k 핑크 골드', val: '15.5cm+2.5cm (+49900원)'}, 
+        {key: '15.5cm+2.5cm||53900||18k 핑크 골드', val: '15.5cm+2.5cm (+53900원)'}
+    ];
+    let jw18k = [
+        {key: '', val:'사이즈를 선택하세요.'},
+        {key: '15.5cm+2.5cm||41900||18k 화이트 골드', val: '15.5cm+2.5cm (+41900원)'}, 
+        {key: '16.5cm+2.5cm||41900||18k 화이트 골드', val: '16.5cm+2.5cm (+41900원)'}, 
+        {key: '17.5cm+2.5cm||45900||18k 화이트 골드', val: '17.5cm+2.5cm (+45900원)'}, 
+        {key: '18.5cm+2.5cm||49900||18k 화이트 골드', val: '15.5cm+2.5cm (+49900원)'}, 
+        {key: '15.5cm+2.5cm||53900||18k 화이트 골드', val: '15.5cm+2.5cm (+53900원)'}
+    ];
+    let val = e.value;
+    let pr2 = document.getElementById('pr2');
+    pr2.options.length  = 0;
+    console.log(val);
+    if(val != ""){
+       switch(val){
+        case 'jy14k':   
+          for(let i in jy14k) {
+              console.log(jy14k[i].key, jy14k[i].val);
+              let opt = document.createElement('option');
+              opt.setAttribute('value', jy14k[i].key);
+              opt.innerText = jy14k[i].val;
+              pr2.appendChild(opt);
+          }
+        break;
+        case 'jp14k':   
+        for(let i in jp14k) {
+            //console.log(j14k[i].key, j14k[i].val);
+            let opt = document.createElement('option');
+            opt.setAttribute('value', jp14k[i].key);
+            opt.innerText = jp14k[i].val;
+            pr2.appendChild(opt);
+        }
+        break;       
+        case 'jw14k':   
+          for(let i in jw14k) {
+              //console.log(j14k[i].key, j14k[i].val);
+              let opt = document.createElement('option');
+              opt.setAttribute('value', jw14k[i].key);
+              opt.innerText = jw14k[i].val;
+              pr2.appendChild(opt);
+          }
+        break;        
+
+        case 'jy18k':
+          for(let i in jy18k) {
+            //console.log(j18k[i].key, j18k[i].val);
+            let opt = document.createElement('option');
+            opt.setAttribute('value', jy18k[i].key);
+            opt.innerText = jy18k[i].val;
+            pr2.appendChild(opt);
+          }
+        break;
+        case 'jp18k':
+          for(let i in jp18k) {
+            //console.log(j18k[i].key, j18k[i].val);
+            let opt = document.createElement('option');
+            opt.setAttribute('value', jp18k[i].key);
+            opt.innerText = jp18k[i].val;
+            pr2.appendChild(opt);
+          }
+        break;
+        case 'jw18k':
+          for(let i in jw18k) {
+            //console.log(j18k[i].key, j18k[i].val);
+            let opt = document.createElement('option');
+            opt.setAttribute('value', jw18k[i].key);
+            opt.innerText = jw18k[i].val;
+            pr2.appendChild(opt);
+          }
+        break;                  
+       }
+    } 
+}
+
+function opt2Change(e) {
+    let val = e.value;
+    let vals = val.split("||");
+    let prTitle = document.getElementsByClassName("item_title")[0].innerHTML;
+    let pprice = document.getElementById('pprice').value;
+    
+    prTitle = `
+         <div class="vbox">
+             <p class="product-title">${prTitle}</p>
+             <span>${vals[2]} / ${vals[0]}</span>
+         </div>
+         <div class="addbox">
+            
+         </div>
+         <div class="prbox">
+             ${priceToString(Number(pprice) + Number(vals[1]))} 원 
+             <p class="point">( <span>P</span> 500원 )</p>
+         </div>
+    `;
+    document.getElementById('total').classList.add('total-inner');
+    document.getElementById('total').innerHTML = prTitle;
+    document.getElementById('total-price').innerHTML = priceToString(Number(pprice) + Number(vals[1])) + '원';
+    // console.log('사이즈' + vals[0]);
+    // console.log('가격' + vals[1]);
+    // console.log('종류' + vals[2]);
+    // let divinn = "";
+    // let totalbox = document.getElementById('total');
+    // totalbox.innerHTML = div; 
+}
+
+function priceToString(price) {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
--- Web/HTML/myweb05/js/script.js
+++ Web/HTML/myweb05/js/script.js
@@ -60,19 +60,160 @@ function fadeInOut(elem, disp) {
        elem.classList.add('fadeIn');
     }   
 }
-function select(){
+function select1(e){
     let pr1 = document.getElementById("pr1");
-    console.log(pr1.options[pr1.selectedIndex].value);
+    let pr2 = document.getElementById("pr2");
+    // console.log(pr1.options[pr1.selectedIndex].value);
     // let pr1_select =pr1.options[pr1.selectedIndex].value;
     // let select1 = pr1_select.split(' ');
     let select1 = pr1.options[pr1.selectedIndex].value.split(' ');
-    console.log(select1[0])
-    if(select1[0] == "14K"){
-
-
-    }else if(select1[0] == "18K"){
-
-
-    }
+    // console.log(select1[0]);
+    document.getElementById('total').innerHTML = '';
+    document.getElementById('total-price').innerHTML = 0;
+    pr2.options.length  = 0;
     
+    let jy14K = [
+        {key: '',val: '- [필수]옵션을 선택해 주세요 -'},
+        {key: '',val: '------------------------------------------'},
+        {key: '15.5cm+2.5cm||0||14K 옐로우 골드', val: '15.5cm+2.5cm'},
+        {key: '16.5cm+2.5cm||0||14K 옐로우 골드', val: '16.5cm+2.5cm'},
+        {key: '17.5cm+2.5cm||3000||14K 옐로우 골드', val: '15.5cm+2.5cm (+3,000원)'},
+        {key: '18.5cm+2.5cm||6000||14K 옐로우 골드', val: '15.5cm+2.5cm (+6,000원)'},
+        {key: '19.5cm+2.5cm||9000||14K 옐로우 골드', val: '15.5cm+2.5cm (+9,000원)'},
+    ];
+    let jp14K = [
+        {key: '',val: '- [필수]옵션을 선택해 주세요 -'},
+        {key: '',val: '------------------------------------------'},
+        {key: '15.5cm+2.5cm||0||14K 핑크 골드', val: '15.5cm+2.5cm'},
+        {key: '16.5cm+2.5cm||0||14K 핑크 골드', val: '16.5cm+2.5cm'},
+        {key: '17.5cm+2.5cm||3000||14K 핑크 골드', val: '15.5cm+2.5cm (+3,000원)'},
+        {key: '18.5cm+2.5cm||6000||14K 핑크 골드', val: '15.5cm+2.5cm (+6,000원)'},
+        {key: '19.5cm+2.5cm||9000||14K 핑크 골드', val: '15.5cm+2.5cm (+9,000원)'},
+    ];
+    let jw14K = [
+        {key: '',val: '- [필수]옵션을 선택해 주세요 -'},
+        {key: '',val: '------------------------------------------'},
+        {key: '15.5cm+2.5cm||0||14K 화이트 골드', val: '15.5cm+2.5cm'},
+        {key: '16.5cm+2.5cm||0||14K 화이트 골드', val: '16.5cm+2.5cm'},
+        {key: '17.5cm+2.5cm||3000||14K 화이트 골드', val: '15.5cm+2.5cm (+3,000원)'},
+        {key: '18.5cm+2.5cm||6000||14K 화이트 골드w', val: '15.5cm+2.5cm (+6,000원)'},
+        {key: '19.5cm+2.5cm||9000||14K 화이트 골드', val: '15.5cm+2.5cm (+9,000원)'},
+    ];
+    // *****************18K*****************
+    let jy18K = [
+        {key: '',val: '- [필수]옵션을 선택해 주세요 -'},
+        {key: '',val: '------------------------------------------'},
+        {key: '15.5cm+2.5cm||41900||18K 옐로우 골드', val: '15.5cm+2.5cm (+41,900원)'},
+        {key: '16.5cm+2.5cm||41900||18K 옐로우 골드', val: '16.5cm+2.5cm (+41,900원)'},
+        {key: '17.5cm+2.5cm||45900||18K 옐로우 골드', val: '15.5cm+2.5cm (+45,900원)'},
+        {key: '18.5cm+2.5cm||49900||18K 옐로우 골드', val: '15.5cm+2.5cm (+49,900원)'},
+        {key: '19.5cm+2.5cm||53900||18K 옐로우 골드', val: '15.5cm+2.5cm (+53,900원)'},
+    ];
+    let jp18K = [
+        {key: '',val: '- [필수]옵션을 선택해 주세요 -'},
+        {key: '',val: '------------------------------------------'},
+        {key: '15.5cm+2.5cm||41900||18K 핑크 골드', val: '15.5cm+2.5cm (+41,900원)'},
+        {key: '16.5cm+2.5cm||41900||18K 핑크 골드', val: '16.5cm+2.5cm (+41,900원)'},
+        {key: '17.5cm+2.5cm||45900||18K 핑크 골드', val: '15.5cm+2.5cm (+45,900원)'},
+        {key: '18.5cm+2.5cm||49900||18K 핑크 골드', val: '15.5cm+2.5cm (+49,900원)'},
+        {key: '19.5cm+2.5cm||53900||18K 핑크 골드', val: '15.5cm+2.5cm (+53,900원)'},
+    ];
+    let jw18K = [
+        {key: '',val: '- [필수]옵션을 선택해 주세요 -'},
+        {key: '',val: '------------------------------------------'},
+        {key: '15.5cm+2.5cm||41900||18K 화이트 골드', val: '15.5cm+2.5cm (+41,900원)'},
+        {key: '16.5cm+2.5cm||41900||18K 화이트 골드', val: '16.5cm+2.5cm (+41,900원)'},
+        {key: '17.5cm+2.5cm||45900||18K 화이트 골드', val: '15.5cm+2.5cm (+45,900원)'},
+        {key: '18.5cm+2.5cm||49900||18K 화이트 골드', val: '15.5cm+2.5cm (+49,900원)'},
+        {key: '19.5cm+2.5cm||53900||18K 화이트 골드', val: '15.5cm+2.5cm (+53,900원)'},
+    ];
+    if(select1[0] == "14K"){
+        switch(select1[1]){
+            case '옐로우':
+                for(let i in jy14K){
+                    let opt = document.createElement('option');
+                    opt.setAttribute('value', jy14K[i].key);
+                    opt.innerText = jy14K[i].val;
+                    pr2.appendChild(opt);
+                }
+            break;
+            case '핑크':
+                for(let i in jp14K){
+                    let opt = document.createElement('option');
+                    opt.setAttribute('value', jp14K[i].key);
+                    opt.innerText = jp14K[i].val;
+                    pr2.appendChild(opt);
+                }
+            break;
+            case '화이트':
+                for(let i in jw14K){
+                    let opt = document.createElement('option');
+                    opt.setAttribute('value', jw14K[i].key);
+                    opt.innerText = jw14K[i].val;
+                    pr2.appendChild(opt);
+                }
+            break;
+        }
+    
+    }else if(select1[0] == "18K"){
+        switch(select1[1]){
+            case '옐로우':
+                for(let i in jy18K){
+                    let opt = document.createElement('option');
+                    opt.setAttribute('value', jy18K[i].key);
+                    opt.innerText = jy18K[i].val;
+                    pr2.appendChild(opt);
+                }
+            break;
+            case '핑크':
+                for(let i in jy18K){
+                    let opt = document.createElement('option');
+                    opt.setAttribute('value', jp18K[i].key);
+                    opt.innerText = jp18K[i].val;
+                    pr2.appendChild(opt);
+                }
+            break;
+            case '화이트':
+                for(let i in jy18K){
+                    let opt = document.createElement('option');
+                    opt.setAttribute('value', jw18K[i].key);
+                    opt.innerText = jw14K[i].val;
+                    pr2.appendChild(opt);
+                }
+            break;
+        }
+    }
+}
+function select2(e){
+    let val = e.value;
+    let vals = val.split("||");
+    let prTitle = document.getElementsByClassName("item_title")[0].innerHTML;
+    let pprice = document.getElementById("pprice").value;
+
+    prTitle = `
+         <div class="vbox">
+             <p class="product-title">${prTitle}</p>
+             <span>${vals[2]} / ${vals[0]}</span>
+         </div>
+         <div class="addbox">
+            
+         </div>
+         <div class="prbox">
+             ${priceToString(Number(pprice) + Number(vals[1]))} 원 
+             <p class="point">( <span>P</span> 500원 )</p>
+         </div>
+    `;
+    document.getElementById('total').classList.add('total-inner');
+    document.getElementById('total').innerHTML = prTitle;
+    document.getElementById('total-price').innerHTML = priceToString(Number(pprice) + Number(vals[1])) + '원';
+
+    // console.log('사이즈'+val[0]);
+    // console.log('가격'+val[1]);
+    // console.log('종류'+val[2]);
+    // let divinn = "<div class='total-inner'></div>";
+    // document.getElementById("total").innerHTML = divinn;
+}
+
+function priceToString(price) {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
