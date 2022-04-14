49ab256b7db7df0d9727ad040234b77bc6a30389
HeeWon <sun4472@naver.com>
2022-04-14 12:40:20 +0900

    update

--- Web/HTML/MoonBucks/js/script.js
+++ Web/HTML/MoonBucks/js/script.js
@@ -3,4 +3,26 @@ $(function(){
         e.preventDefault();
         $('.searchForm,.input-search').toggleClass('act');
     });
+
+    $('.gnb>li').hover(function(){
+        $(this).find('.lnb').slideToggle();
+    });
+
+    $('.mobile-nav-box>li').click(function(){
+        if($(this).find('.fa-solid').hasClass('fa-angle-down')){
+            $(this).find('.fa-solid').removeClass('fa-angle-down').addClass('fa-angle-up');
+        }else{
+            $(this).find('.fa-solid').removeClass('fa-angle-up').addClass('fa-angle-down');
+        }
+        $(this).find('.mobile-lnb').slideToggle();
+    });
+    $('.btn-box>.fa-bars').click(function(){
+        $('.fadeblack').css('right', '0%');
+        $('.box').css('left', '-70%');
+    });
+    $('.xclose').click(function(){
+        $('.box').css('left', '0%');
+        $('.fadeblack').css('right', '-100%');
+    });
 });
+
