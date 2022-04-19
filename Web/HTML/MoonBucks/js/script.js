eba8df72eb1ac16f5d31d21b1178dab157b5a06c
HeeWon <sun4472@naver.com>
2022-04-19 15:44:47 +0900

    update

--- Web/HTML/MoonBucks/js/script.js
+++ Web/HTML/MoonBucks/js/script.js
@@ -48,5 +48,33 @@ $(function(){
        arrows: false
     });
   
+    $('.imgSliderClass').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+
+
+
 });
 
