d035fde4950069c9088a08b182be075fb8f56a54
HeeWon <sun4472@naver.com>
2022-04-29 14:34:13 +0900

    2022-04-29

--- Web/React/chk/src/movie_detail/Movie_Page_Slider.jsx
+++ Web/React/chk/src/movie_detail/Movie_Page_Slider.jsx
@@ -1,50 +1,38 @@
-import { ArrowBackIos, ArrowForwardIos } from '@styled-icons/material'
-import './movie_page_slider.css'
-
-import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-coverflow";
+import { Autoplay, EffectCoverflow, Pagination, Navigation } from "swiper";
 
 const Movie_Page_Slider = () => {
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 1000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    // autoplay: false,
-    // nextArrow: <ArrowForwardIos/>,
-    // prevArrow:<ArrowBackIos />
-  };
   return (
-    <>
-    <Slider {...settings}>
-      <div className='H_Slider'>
-          <img src="images/slider/d-1.jpg" alt="" />
-      </div>
-      <div className='H_Slider'>
-          <img src="images/slider/d-2.jpg" alt="" />
-      </div>
-      <div className='H_Slider'>
-          <img src="images/slider/d-3.jpg" alt="" />
-      </div>
-      <div className='H_Slider'>
-          <img src="images/slider/d-4.jpg" alt="" />
-      </div>
-      <div className='H_Slider'>
-          <img src="images/slider/d-5.jpg" alt="" />
-      </div>
-    </Slider>
-    {/* <div className='H_Movie_Page_Poster_Box'>
-      <button className='H_Movie_Back_Btn'><ArrowBackIos /></button>
-      <button className='H_Movie_Forward_Btn'><ArrowForwardIos/></button>
-      <div className="H_Movie_Page_Poster_View_Box">
-        <div className="H_Movie_Page_Poster">
-        </div>
-      </div>
-    </div> */}
-    </>
+    <Swiper
+    effect={"coverflow"}
+    grabCursor={true}
+    navigation={true}
+    pagination={{
+      type: "fraction",
+    }}
+    autoplay={{
+      delay: 5000
+    }}
+    coverflowEffect={{
+      rotate: 100,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    }}
+    modules={[Autoplay, EffectCoverflow, Pagination, Navigation]}
+    className="mySwiper"
+    >
+      <SwiperSlide><img className='H_Movie_Page_Slider' src="images/slider/d-1.jpg" alt="" /></SwiperSlide>
+      <SwiperSlide><img className='H_Movie_Page_Slider' src="images/slider/d-2.jpg" alt="" /></SwiperSlide>
+      <SwiperSlide><img className='H_Movie_Page_Slider' src="images/slider/d-3.jpg" alt="" /></SwiperSlide>
+      <SwiperSlide><img className='H_Movie_Page_Slider' src="images/slider/d-4.jpg" alt="" /></SwiperSlide>
+      <SwiperSlide><img className='H_Movie_Page_Slider' src="images/slider/d-5.jpg" alt="" /></SwiperSlide>
+    </Swiper>
   )
 }
 
