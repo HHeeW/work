76c1246ca9479f36f6cccc4424a03079687d1242
HeeWon <sun4472@naver.com>
2022-05-16 19:02:45 +0900

    2022-05-16

--- Web/React/chk/src/H_Movie/movie_detail/Movie_Page_Review.jsx
+++ Web/React/chk/src/H_Movie/movie_detail/Movie_Page_Review.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Movie_Page_Review = () => {
+  const [Heart, setHeart] = useState(false);
+  const [grade, setGrade] = useState(0);
+    const HeartClick = (e) => {
+      setHeart(!Heart)
+      Heart ? e.target.className = "fa-regular fa-heart H_Heart_active" :
+              e.target.className = "fa-regular fa-heart";
+    }
   return (
     <div className='H_Movie_Page_Review'>
         <div className="H_Review_WriteBox">
             <div className='H_Review_grade'>
-                <p>평점</p>
+                <p> {grade} 평점 </p>
                 <div className="H_point">
-                   <i class="fa-regular fa-heart"><i class="fa-regular fa-heart"><i class="fa-regular fa-heart"><i class="fa-regular fa-heart"><i class="fa-regular fa-heart"><i class="fa-regular fa-heart"><i class="fa-regular fa-heart"><i class="fa-regular fa-heart"><i class="fa-regular fa-heart"><i class="fa-regular fa-heart"></i></i></i></i></i></i></i></i></i></i> 
+                   <i class="fa-regular fa-heart" onClick={HeartClick} id={1}>
+                   <i class="fa-regular fa-heart" onClick={HeartClick} id={2}>
+                   <i class="fa-regular fa-heart" onClick={HeartClick} id={3}>
+                   <i class="fa-regular fa-heart" onClick={HeartClick} id={4}>
+                   <i class="fa-regular fa-heart" onClick={HeartClick} id={5}>
+                   <i class="fa-regular fa-heart" onClick={HeartClick} id={6}>
+                   <i class="fa-regular fa-heart" onClick={HeartClick} id={7}>
+                   <i class="fa-regular fa-heart" onClick={HeartClick} id={8}>
+                   <i class="fa-regular fa-heart" onClick={HeartClick} id={9}>
+                   <i class="fa-regular fa-heart" onClick={(e)=>{}} id={10}>
+                   </i></i></i></i></i></i></i></i></i></i> 
                 </div>
             </div>
             <textarea type="text" className='H_Review_Text' cols={30} rows={3} maxLength={110} required
