9658b380b57804f4521e85e2e066a4d7703bf800
HeeWon <sun4472@naver.com>
2022-05-18 13:41:55 +0900

    2022-05-18

--- Web/React/my-study-9/src/App.js
+++ Web/React/my-study-9/src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
 import ReactProgress from './component/ReactProgress'
+import Sweetalert from './component/Sweetalertbasic'
 
 const App = () => {
   return (
     <div>
       <h1>Start React !!!</h1>
       <ReactProgress/>
+      <Sweetalert/>
     </div>
   )
 }
