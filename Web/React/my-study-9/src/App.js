2edf56b169afabc88b8c15886563a251823badb4
HeeWon <sun4472@naver.com>
2022-05-18 11:33:12 +0900

    2022-05-18

---
+++ Web/React/my-study-9/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import './App.css'
+import ReactProgress from './component/ReactProgress'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Start React !!!</h1>
+      <ReactProgress/>
+    </div>
+  )
+}
+
+export default App
