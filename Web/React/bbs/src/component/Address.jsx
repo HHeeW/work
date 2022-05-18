1803322a4a5d2a80439697a65ab262872b85bd0c
HeeWon <sun4472@naver.com>
2022-05-18 15:13:22 +0900

    2022-05-18

--- Web/React/bbs/src/component/Address.jsx
+++ Web/React/bbs/src/component/Address.jsx
@@ -2,17 +2,13 @@ import React, { useState } from 'react'
 import PopupDom from '../lib/PopupDom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationPin } from '@fortawesome/free-solid-svg-icons';
+import { Modal } from 'react-bootstrap';
 
 const Address = () => {
   //팝업의 상태를 관리
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   //팝업창 열기 
-  const openPop = () => {
-      setIsPopupOpen(true);
-  }  
-  const closePop = () => {
-      setIsPopupOpen(false);
-  }
+  const toggle =()=>{setModal(!modal);}
   return (
     <>  
     <div className="input-group mb-3">
@@ -21,7 +17,7 @@ const Address = () => {
          <button className="btn btn-success" onClick={openPop}>주소검색</button>
          <input type="text" className="form-control" readonly />
     </div>
-    <div id="popupDom">
+    <Modal is Open={modal} toggle={toggle}>
         {
             isPopupOpen && (
                 <PopupDom>
@@ -29,7 +25,7 @@ const Address = () => {
                 </PopupDom>
             )
         }
-    </div>
+    </Modal>
     </>
   )
 }
