b3bcd96e2007692d58c50409a9d559c04a7e171d
HeeWon <sun4472@naver.com>
2022-05-24 09:18:57 +0900

    2022-05-20

---
+++ Web/React/bank-test/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Body from './body/Body'
+import Top from './head/Top'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Top/>
+      <Body />
+    </div>
+  )
+}
+
+export default App
