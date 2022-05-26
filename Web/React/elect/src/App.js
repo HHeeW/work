9354e0fa175caf042ca81082789dad3bd186c07b
HeeWon <sun4472@naver.com>
2022-05-26 19:43:34 +0900

    2022-05-26

--- Web/React/elect/src/App.js
+++ Web/React/elect/src/App.js
@@ -2,20 +2,26 @@ import React, { useEffect, useState } from 'react'
 import Maps from './kakaomap/Maps'
 import Lists from './kakaomap/Lists'
 const App = () => {
+
+  const [myLat, setMylat] = useState()
+  const [myLng, setMylng] = useState()
+  const [lat, setLat] = useState(37.571076);
+  const [lng, setLng] = useState(126.99588);
+
 useEffect(()=>{
   if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position){
-        setLat(position.coords.latitude);
-        setLng(position.coords.longitude);
+    navigator.geolocation.getCurrentPosition((position)=>{
+      setMylat(position.coords.latitude);
+      setMylng(position.coords.longitude);
+      setLat(position.coords.latitude);
+      setLng(position.coords.longitude);
     });
   }
 }, [])
-  const [lat, setLat] = useState(37.571076);
-  const [lng, setLng] = useState(126.99588);
-  console.log(lat, lng)
+console.log(myLng, myLng)
   return (
     <div>
-      <Maps lat={lat} lng={lng}/>
+      <Maps lat={lat} lng={lng} MyLat = {myLat} MyLng={myLng} />
       <Lists setLat={setLat} setLng={setLng} />
     </div>
   )
--- Web/React/elect/src/kakaomap/Lists.jsx
+++ Web/React/elect/src/kakaomap/Lists.jsx
@@ -5,7 +5,7 @@ import Charger from './Charger';
 const Lists = (props) => {
   return (
     <>
-      <h1 className="text-center" style={{width: "20%", lineHeight:"10vh"}}> 전기차 충선소 위치</h1>
+      <h1 className="text-center" style={{width: "20%", lineHeight:"9vh"}}> 전기차 충선소 위치</h1>
         <ListGroup numbered style={{height: "90vh", overflowY: "scroll"}}>
           <Charger setLat={props.setLat} setLng={props.setLng}/>
         </ListGroup>
