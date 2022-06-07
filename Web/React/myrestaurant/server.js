a09b41c8815076b535b3cd3f2b67e6b7d47ce5a7
HeeWon <sun4472@naver.com>
2022-06-07 15:45:12 +0900

    2022-06-07

--- Web/React/myrestaurant/server.js
+++ Web/React/myrestaurant/server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,8 +7,25 @@ const port = process.env.PORT || 4000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/app/hello', (req, res) =>{
-    res.send({message : 'Welcom NodeJs Server'});
+const data = fs.readFileSync('./db/db.json')
+const conf = JSON.parse(data);
+const con = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+con.connect();
+
+app.get('/app', (req, res) =>{
+    let offset = 0;
+    let limits = 30;
+    let dbquery = `SELECT * FROM restaurant_ggy order by id desc LIMIT ${offset}, ${limits}`;
+    con.query(dbquery, (err, rows, fields)=>{
+        if(err){console.log('db접속중에 에러가 발생했습니다.' + err)}
+        res.send(rows);
+    })
 })
 
 app.listen(port, () => console.log(`Listening on Port ${port}`));
