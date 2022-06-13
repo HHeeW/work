c37c505a941f933ada313d0cf1bd3234fbae8cf6
HeeWon <sun4472@naver.com>
2022-06-13 14:10:09 +0900

    2022-06-13

--- Web/React/myrestaurant/client/src/App.js
+++ Web/React/myrestaurant/client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import RestList from './component/RestList';
+import RestWrite from './component/RestWrite';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+
 const App = () => {
 
   return (
     <div>
-      <RestList/>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<RestList/>}/>
+          <Route path={"write"} element={<RestWrite/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ Web/React/myrestaurant/client/src/component/RestWrite.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RestWrite = () => {
+  return (
+    <div>RestWrite</div>
+  )
+}
+
+export default RestWrite
