fbde9885343524eb3e7ac47bb18f41ad9d020c64
HeeWon <sun4472@naver.com>
2022-06-16 12:40:45 +0900

    2022-06-16

--- Web/Node/class12/models/user.js
+++ Web/Node/class12/models/user.js
@@ -1,17 +1,4 @@
 const Sequelize = require('sequelize');
-/*
-    mysql to sequelize 자료형 비교
-    auto_increment : autoIncrement: true,
-    primart key : primaryKey: true
-    varchar(100) -> Sequelize.STRING(100)
-    init -> Sequelize.INTEGER,
-    tinyint -> Sequelize.BOOLEAN,
-    datetime -> Sequelize.DATE,
-    int unmsigned -> Sequelize.INTEGER.UNSIGNED,
-    not null -> allowNull: false
-    unique -> unique: true,
-    default now() -> defaultValue: Sequelize
-*/
 
 module.exports = class User extends Sequelize.Model{
     static init(sequelize){
@@ -52,8 +39,8 @@ module.exports = class User extends Sequelize.Model{
         sequelize,
         timestamps: false,
         underscored: false,
-        modelName: 'Member',
-        tableName: 'members',
+        modelName: 'User',
+        tableName: 'users',
         paranoid: false,
         charset: 'utf8',
         collate: 'utf8_general_ci'
