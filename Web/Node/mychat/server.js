9b1f9ae751ba8b0cd74206a659cf468adbebb37b
HeeWon <sun4472@naver.com>
2022-07-01 09:35:37 +0900

    2022-07-01

---
+++ Web/Node/mychat/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const mongoose = require('mongoose')
+
+let database;
+let UserSchema;
+let UserModel;
+
+const connectDB = () =>{
+    const database = 'mongode://127.0.0.1:27017/local'
+    mongoose.Promise = global.Promise;
+    mongoose.connect(databaseUrl);
+    database = mongoose.connection;
+    database.on('open',()=>{
+        
+    })
+}
+//에러 핸들러 모듈
+//const expressErrorHandler = require('express-error-handler');
+const app = express();
+app.setMaxListeners('port',4000);
+
+//static
+app.use('/', static(path.join(__dirname,'public')));
+
+//bodyparser
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
--- Web/React/mysns/passport/index.js
+++ Web/React/mysns/passport/index.js
@@ -2,3 +2,7 @@ const passport = require('passport');
 const local = require('./localStrategy');
 const kakao = require('./kakaoStrategy');
 const Member = require('./models/member');
+
+module.exports = () =>{
+    
+}
