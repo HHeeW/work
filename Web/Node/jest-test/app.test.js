623ad6b988489051425c6087cb5073447b728be7
HeeWon <sun4472@naver.com>
2022-07-08 14:13:55 +0900

    2022-07-08

--- Web/Node/jest-test/app.test.js
+++ Web/Node/jest-test/app.test.js
@@ -1,19 +1,10 @@
-test('2 더하기 2는 4가 됩니다.', ()=>{
-    expect(2 + 2).toBe(4);
-})
 
-test('object assignment', ()=>{
-    const data = {one : 1};
-    data['two'] = 2;
-    expect(data).toEqual({one: 1, two: 2})
-})
-
-//함수 제작
-import { addNumber } from "./addNumbert";
+const {addNumber} = require('./addNumber');
 test("두 수를 더해주는 함수 addNumber", ()=>{
     const a = 3;
     const b = 7;
     const result = 10;
     //실행
-    const actResult = addNumber(a + b)
+    const actResult = addNumber(a, b);
+    expect(actResult).toBe(result);
 })
