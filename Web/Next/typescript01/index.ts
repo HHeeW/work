956a599497a52eab1c981618f98c90a6672b4c22
HeeWon <sun4472@naver.com>
2022-07-13 10:14:06 +0900

    2022-07-13

---
+++ Web/Next/typescript01/index.ts
@@ -0,0 +1,23 @@
+const user :string = 'hong';
+let member :string[] = ['kim', 'hong', 'lee'];
+let uname : {name: string} = {name: 'hong'};
+
+type mytype = string | number;
+let str : mytype = 1234;
+
+function myfunc(x:number):number{
+    return x * 3;
+
+}
+
+/*
+    string
+    number
+    boolean
+    null
+    nudefined
+    bigint
+    []
+    {}
+    사용자
+*/
---
+++ Web/Node/HELLO_NEXT/pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+const Index = () => (
+    <div>
+        <Link href={"/about"}>
+            <button>Hello Next!!!</button>
+        </Link>
+    </div>
+)
+
+export default Index;
