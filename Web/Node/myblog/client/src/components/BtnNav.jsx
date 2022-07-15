c625570462be32f63cc5a1494a1e4b2e11ee0433
HeeWon <sun4472@naver.com>
2022-07-15 10:40:54 +0900

    2022-07-15

---
+++ Web/Node/myblog/client/src/components/BtnNav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BtnNav = () => {
+  return (
+    <div>
+        <button>목록보기</button>
+        <button></button>
+        <button></button>
+    </div>
+  )
+}
+
+export default BtnNav
