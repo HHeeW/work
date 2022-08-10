5d703dfdaa876e81486b38a0be7233a0e53a6991
HeeWon <sun4472@naver.com>
2022-08-10 09:38:33 +0900

    2022-08-10

---
+++ App/React_Native/StyleApp/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ScrollView } from 'react-native'
+import Style01 from './components/Style01'
+
+
+export const App = () => {
+  return (
+    <ScrollView>
+      <Style01/>
+    </ScrollView>
+  )
+}
---
+++ App/React_Native/StyleApp/components/Style01.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+const Style01 = () => {
+    return (
+        <View style={{marginTop:60, marginLeft:30}}>
+            <Text style={{fontSize:20, color:'red', fontWeight:'bold'}}>스타일시트 적용</Text>
+        </View>
+    )
+}
+
+const st =
