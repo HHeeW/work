ff6172dd650602f4531e31f819131dbc8582ce1b
HeeWon <sun4472@naver.com>
2022-09-13 17:30:23 +0900

    2022-09-13

---
+++ App/React_Native/Golf/navigation/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AuthProvider} from './AuthProvider'
+import { Routes } from './Routes'
+
+const Providers = () => {
+    <AuthProvider>
+        <Routes/>
+    </AuthProvider>
+}
+export default Providers
